a1b26d405d7aca8c22c2fe997ce9f4bb7ab52c7f
Alex <alex.rf.miguel00@gmail.com>
2025-03-04 22:44:57 -0500

    lint

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -1,28 +1,28 @@
 // next-auth.d.ts
-// import NextAuth, { DefaultSession } from "next-auth";
+import { DefaultSession } from "next-auth";
 // import { JWT } from "next-auth/jwt";
 
-// declare module "next-auth" {
-//   interface Session {
-//     user: {
-//       /** The user's unique identifier. */
-//       id?: string;
-//       /** The user's role. */
-//       role?: string;
-//     } & DefaultSession["user"];
-//   }
+declare module "next-auth" {
+  interface Session {
+    user: {
+      /** The user's unique identifier. */
+      id?: string;
+      /** The user's role. */
+      role?: string;
+    } & DefaultSession["user"];
+  }
 
-//   interface User {
-//     /** The user's unique identifier. */
-//     id?: string;
-//     /** The user's role. */
-//     role?: string;
-//   }
-// }
+  interface User {
+    /** The user's unique identifier. */
+    id?: string;
+    /** The user's role. */
+    role?: string;
+  }
+}
 
-// declare module "next-auth/jwt" {
-//   interface JWT {
-//     /** The user's role. */
-//     role?: string;
-//   }
-// }
+declare module "next-auth/jwt" {
+  interface JWT {
+    /** The user's role. */
+    role?: string;
+  }
+}
